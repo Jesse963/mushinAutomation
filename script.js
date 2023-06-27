a889ce37f61d4a39343663ef923af24606fd11a4
Jesse Jenkins <jesse-jenkins@hotmail.com>
2023-06-27 12:43:50 +1000

    htaccess added

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ window.onload = function () {
   console.log(extendedDecoration);
 };
 
+//ADD HOME NAV TO ALL LOGOS
+document.querySelectorAll("logo").forEach((logo) => {
+  logo.addEventListener("click", (e) => {
+    window.location.href = "/";
+  });
+});
+
+//ADD NAV TO ALL NAVLINKS
+document.querySelectorAll(".navLink").forEach((link) => {
+  console.log(link.textContent);
+  const targetPage = link.textContent.replace(" ", "_").toLowerCase();
+  link.addEventListener("click", (e) => {
+    window.location.href = "/" + targetPage;
+  });
+});
+
 // INTERSECTION OBSERVER
 const navbarMenu = document.querySelector("navbar");
 const options = {
@@ -22,16 +38,10 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       navbarMenu.style.transform = "translate(-50%, -150%)";
-      // navbarMenu.style.visibility = "hidden";
-      console.log("Hiding menu");
     } else {
-      console.log("Showing menu");
       navbarMenu.style.transform = "translate(-50%, 0%)";
-      // navbarMenu.style.visibility = "visible";
     }
   });
 }, options);
 
-const target = document.querySelector(".main.logo");
-
-observer.observe(target);
+observer.observe(document.querySelector(".main.logo"));
